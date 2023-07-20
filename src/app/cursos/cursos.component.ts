aba33fcb46755ff6cb7ab57c0ea82c3826aa82f0
JhonHTrujillo <jhonhtg90@gmail.com>
2023-07-20 17:27:49 -0500

    Feat: creacion componente cursos

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
 
+interface CardItem {
+  titulo: String;
+  imagen: String;
+  descripcion: String;
+  enlace: String;
+}
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent {
-
+  itemsCard: CardItem[] = [{
+    titulo:'Hapkido',
+    imagen:'../../assets/images/hapkido.jpg',
+    descripcion:'Arte marcial coreano',
+    enlace:'hapkido.html',
+},
+{
+    titulo:'Artistica',
+    imagen:'../../assets/images/artistica.jpg',
+    descripcion:'Creaciones Unicas',
+    enlace:'artistica.html',
+},
+{
+    titulo:'Tecnologia',
+    imagen:'../../assets/images/tecnologia.jpeg',
+    descripcion:'Conocimientos y habilidades',
+    enlace:'tecnologia.html',
+},
+{
+    titulo:'Educacion Fisica',
+    imagen:'../../assets/images/educacionfisica.jpg',
+    descripcion:'Disciplina pedagogica',
+    enlace:'edufisica.html',
+},
+{
+    titulo:'Ingles',
+    imagen:'../../assets/images/ingles.avif',
+    descripcion:'Lenguaje de comunicacion',
+    enlace:'ingles.html',
+},
+{
+    titulo:'Musica',
+    imagen:'../../assets/images/musica.jpg',
+    descripcion:'Expresion Artistica',
+    enlace:'musica.html',
 }
+  ]
+}
+
+
+
